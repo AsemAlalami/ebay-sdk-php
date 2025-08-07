d83a5d74e2449bc2f852e0a561fbfa40f594f263
Asem Alalami <asem.almi@gmail.com>
2025-08-07 14:39:07 +0300

    Update TradingBaseService.php

--- src/Trading/Services/TradingBaseService.php
+++ src/Trading/Services/TradingBaseService.php
@@ -160,7 +160,8 @@ class TradingBaseService extends \DTS\eBaySDK\Services\BaseService
          * Note that we have no way of detecting that the actual request has an attachment.
          */
         if ($operationName === 'UploadSiteHostedPictures') {
-            $headers['Content-Type'] = 'multipart/form-data;boundary="boundary"';
+            // I will use ExternalPictureURL
+            // $headers['Content-Type'] = 'multipart/form-data;boundary="boundary"';
         }
 
         return $headers;
